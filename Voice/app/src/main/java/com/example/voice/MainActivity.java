23aea61d2ace1405c5fc119da9bf1bdd13b5ced5
Mad-way <mkaygorodskiy@mail.ru>
2021-10-23 12:32:13 +0300

    Add Voice

---
+++ Voice/app/src/main/java/com/example/voice/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.voice;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView testText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    public void onClickMic(View view)
+    {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        startActivityForResult(intent, 10);
+    }
+
+    private void init()
+    {
+        testText = findViewById(R.id.textTest);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && data != null)
+        {
+            switch (requestCode)
+            {
+                case 10:
+                    ArrayList<String> text =  data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    testText.setText(text.get(0));
+                    break;
+
+            }
+        }
+    }
+}
